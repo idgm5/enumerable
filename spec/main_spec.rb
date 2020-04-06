5de453f1a1d51ff5c49d16a3db8a48846efd69bd
isaac <idgm@pm.me>
2020-04-06 12:54:47 -0500

    Add test for my_each and my_each_with_index

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,14 +2,38 @@
 require './main.rb'
 
 RSpec.describe Enumerable do
+  array = [11, 2, 3, 56]
+
   describe '.my_each' do
-    context 'when receive a block' do
-      it 'returns a sum of all numbers inside of the array' do
-      
+    context 'when receive an array as a block' do
+      it 'push each value to the result array' do
+        result = []
+        array.my_each { |x| result.push(x) }
+        expect(result).to(eq(array))
+      end
+    end
+
+    context 'when no block is given' do
+      it 'returns an enumerator' do
+        expect(array.my_each).to be_an Enumerator
       end
     end
   end
 
+  describe '.my_each_with_index' do
+    context 'when receive an array as a block' do
+      it 'push each value to the result array with index' do
+        result = []
+        array.my_each_with_index { |x, y| result.push([x, y]) }
+        expect(result[0][1]).to(eq(0))
+      end
+    end
 
+    context 'when no block is given' do
+      it 'returns an enumerator' do
+        expect(array.my_each_with_index).to be_an Enumerator
+      end
+    end
+  end
 
 end
