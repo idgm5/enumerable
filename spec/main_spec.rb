49eb9e3018461ab1a534a7371ffd82e130b0fdc3
isaac <idgm@pm.me>
2020-04-06 13:01:16 -0500

    Add my_select method WIP

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Enumerable do
   array = [11, 2, 3, 56]
 
   describe '.my_each' do
-    context 'when receive an array as a block' do
+    context 'when receive a block' do
       it 'push each value to the result array' do
         result = []
         array.my_each { |x| result.push(x) }
@@ -21,7 +21,7 @@ RSpec.describe Enumerable do
   end
 
   describe '.my_each_with_index' do
-    context 'when receive an array as a block' do
+    context 'when receive a block' do
       it 'push each value to the result array with index' do
         result = []
         array.my_each_with_index { |x, y| result.push([x, y]) }
@@ -32,6 +32,20 @@ RSpec.describe Enumerable do
     context 'when no block is given' do
       it 'returns an enumerator' do
         expect(array.my_each_with_index).to be_an Enumerator
+      end
+    end
+  end
+
+  describe '.my_select' do
+    context 'when receive a block' do
+      it 'returns selected values' do
+      
+      end
+    end
+
+    context 'when no block is given' do
+      it 'returns an enumerator' do
+        expect(array.my_select).to be_an Enumerator
       end
     end
   end
