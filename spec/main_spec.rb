70d7228a9aff1bca0e3cd03b69b3ba4663fccdad
isaac <idgm@pm.me>
2020-04-06 12:11:17 -0500

    Add rspec files

---
+++ spec/main_spec.rb
@@ -0,0 +1,15 @@
+# spec :main_spec.rb
+require './main.rb'
+
+RSpec.describe Enumerable do
+  describe '.my_each' do
+    context 'when receive a block' do
+      it 'returns a sum of all numbers inside of the array' do
+      
+      end
+    end
+  end
+
+
+
+end
